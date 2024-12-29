7df6aea39893d247218dd74eaa8870e2cd210d92
Vincent Tonkes <vincenttonkes@gmail.com>
2024-12-29 21:28:18 +0100

    forgot to stage the most important file in previous commit 🙈

--- tests/cpp/exploding_kittens/environment/actions/test_noping.cpp
+++ tests/cpp/exploding_kittens/environment/actions/test_noping.cpp
@@ -169,8 +169,147 @@ TEST(NopingTest, SimplePlayNopeTest) {
         << "Secondary players list should be empty again";
 }
 
-// TODO: noping a nope
-// TODO: dead players with a nope
-// TODO: 2 players: first skips nope, second plays it
+TEST(NopingTest, SimpleDoubleNopeTest) {
+    GameState gs;
+    DummyNopable dn(gs);
+    PlayNope pn(gs);
+    custom_state_reset(gs, 2, [](Cards &c) {
+        c.hands[0].counts()[to_uint(CardIdx::Shuffle)] = 1U;
+        c.hands[0].counts()[to_uint(CardIdx::Nope)] = 1U;
+        c.hands[1].counts()[to_uint(CardIdx::Nope)] = 1U;
+    });
+    auto actions = get_legal_actions(dn);
+    ASSERT_EQ(actions.size(), 1) <<
+        "We should have this action because we have a shuffle";
+
+    dn.take_action(actions[0]);
+    
+    EXPECT_EQ(gs.cards.hands[0].has(CardIdx::Shuffle), 0)
+        << "Player should have lost its shuffle card";
+    EXPECT_EQ(gs.cards.discard_pile.has(CardIdx::Shuffle), 1)
+        << "The shuffle should now be on the discard pile.";
+    
+    ASSERT_EQ(gs.secondary_players.size(), 2)
+        << "2 players have a nope, so 2 players can nope.";
+    
+    actions = get_legal_actions(pn);
+    ASSERT_EQ(actions.size(), 1) << "Secondary can play nope";
+
+    pn.take_action(actions[0]);
+
+    // Checks after first nope:
+    EXPECT_EQ(dn.call_count, 0)
+        << "Enforce_action should not yet have taken place.";
+    EXPECT_EQ(gs.state, State::Nope) << "We should still be in the nope state";
+    ASSERT_EQ(gs.secondary_players.size(), 1)
+        << "One player left that can nope.";
+    EXPECT_EQ(gs.cards.discard_pile.has(CardIdx::Nope), 1)
+        << "This nope should be on the discard pile";
+    
+    actions = get_legal_actions(pn);
+    ASSERT_EQ(actions.size(), 1) << "Secondary can play nope the nope";
+
+    pn.take_action(actions[0]);
+
+    // Checks after second nope:
+    EXPECT_EQ(dn.call_count, 1)
+        << "Nope blocked: action should have happened.";
+    EXPECT_EQ(gs.state, State::Default) << "Should be in default state again.";
+    
+    // Check if nope-related data is valid:
+    EXPECT_EQ(gs.cards.hands[0].has(CardIdx::Nope), 0)
+        << "First player should have lost its nope";
+    EXPECT_EQ(gs.cards.hands[1].has(CardIdx::Nope), 0)
+        << "Second player should have lost its nope too";
+    EXPECT_EQ(gs.secondary_players.size(), 0)
+        << "Secondary players list should be empty again";
+    EXPECT_EQ(gs.cards.discard_pile.has(CardIdx::Nope), 2)
+        << "Both nopes should have been placed on the discard pile";
+}
+
+TEST(NopingTest, DeadPlayerHasNope) {
+    GameState gs;
+    DummyNopable dn(gs);
+    custom_state_reset(gs, 3, [](Cards &c) {
+        c.hands[0].counts()[to_uint(CardIdx::Shuffle)] = 1U;
+        c.hands[1].counts()[to_uint(CardIdx::Nope)] = 1U;
+        c.hands[1].counts()[to_uint(CardIdx::Exploding_Kitten)] = 1U;
+        // Having an exploding kitten in your hand means you are dead.
+    });
+    auto actions = get_legal_actions(dn);
+    ASSERT_EQ(actions.size(), 1) <<
+        "We should have this action because we have a shuffle";
+
+    dn.take_action(actions[0]);
+
+    ASSERT_FALSE(gs.is_alive(1)) << "Make sure player 1 is dead.";
+    EXPECT_EQ(dn.call_count, 1)
+        << "Only noper is dead, so enforce_action should have been called.";
+    EXPECT_EQ(gs.state, State::Default) << "Should be in default state still.";
+    
+    EXPECT_EQ(gs.cards.hands[0].has(CardIdx::Shuffle), 0)
+        << "Player should have lost its shuffle card";
+    EXPECT_EQ(gs.cards.discard_pile.has(CardIdx::Shuffle), 1)
+        << "The shuffle should now be on the discard pile.";
+}
+
+TEST(NopingTest, SkipNopeSkipTest) {
+    GameState gs;
+    DummyNopable dn(gs);
+    PlayNope pn(gs);
+    SkipNope sn(gs);
+    custom_state_reset(gs, 3, [](Cards &c) {
+        c.hands[0].counts()[to_uint(CardIdx::Shuffle)] = 1U;
+        c.hands[1].counts()[to_uint(CardIdx::Nope)] = 1U;
+        c.hands[2].counts()[to_uint(CardIdx::Nope)] = 1U;
+    });
+    auto actions = get_legal_actions(dn);
+    ASSERT_EQ(actions.size(), 1) <<
+        "We should have this action because we have a shuffle";
+
+    dn.take_action(actions[0]);
+
+    ASSERT_EQ(gs.secondary_players.size(), 2) <<
+        "2 players can skip or nope.";
+    
+    // The first player will skip
+    uint8_t first_player = gs.secondary_players.back();
+    actions = get_legal_actions(sn);
+    ASSERT_EQ(actions.size(), 1) << "First player can skip";
+    sn.take_action(actions[0]);
+
+    ASSERT_EQ(gs.secondary_players.size(), 1) <<
+        "1 player left that can skip or nope.";
+    ASSERT_EQ(gs.state, State::Nope) << "We should still be in the nope state";
+
+    // Second player will play action
+    actions = get_legal_actions(pn);
+    ASSERT_EQ(actions.size(), 1) << "Second player can nope still";
+    pn.take_action(actions[0]);
+
+    ASSERT_EQ(gs.secondary_players.size(), 1) <<
+        "Player that skipped now has a new opportunity.";
+    ASSERT_EQ(gs.state, State::Nope) << "We should still be in the nope state";
+    EXPECT_EQ(gs.secondary_players.back(), first_player)
+        << "This should be the same player as before";
+
+    // First player skips again.
+    actions = get_legal_actions(sn);
+    ASSERT_EQ(actions.size(), 1) << "First player can skip";
+    sn.take_action(actions[0]);
+
+    EXPECT_EQ(dn.call_count, 0)
+        << "The action was noped so enforce_action was not called.";
+    EXPECT_EQ(gs.state, State::Default) << "Should be back to default state.";
+    
+    EXPECT_EQ(gs.cards.hands[0].has(CardIdx::Shuffle), 0)
+        << "Player should have lost its shuffle card";
+    EXPECT_EQ(gs.cards.discard_pile.has(CardIdx::Shuffle), 1)
+        << "The shuffle should now be on the discard pile.";
+    EXPECT_EQ(gs.cards.discard_pile.has(CardIdx::Nope), 1)
+        << "Only the nope from the one player is on the pile.";
+    EXPECT_EQ(gs.cards.hands[first_player].has(CardIdx::Nope), 1)
+        << "First player skipped, so should still have its nope.";
+}
 
 } // exploding_kittens
